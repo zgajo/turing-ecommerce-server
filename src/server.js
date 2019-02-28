7eda9dea46b01f5da1ece476a9fc31597b21c44d
Darko Pranjic <darkopranjic.4@gmail.com>
2019-02-28 08:31:58 +0100

    Make product images available globally

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const express = require('express');
 const fs = require('fs');
 const { GraphQLServer } = require('graphql-yoga');
 const models = require('./models');
@@ -10,5 +11,7 @@ const { permissions } = require('./permissions');
 const typeDefs = fs.readFileSync(__dirname + '/schema/Schema.gql', 'utf8');
 
 const server = new GraphQLServer({ context: { models }, permissions, resolvers, typeDefs });
+
+server.express.use('/product_images', express.static('product_images'));
 
 server.start();
