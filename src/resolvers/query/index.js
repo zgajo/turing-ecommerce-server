714f60aa182e77bd1966227830650fb8023d788a
Darko Pranjic <darkopranjic.4@gmail.com>
2019-02-28 17:50:35 +0100

    Category products pagination

--- src/resolvers/query/index.js
+++ src/resolvers/query/index.js
@@ -2,11 +2,14 @@ module.exports = {
 	attribute_values: async (_, __, { models }) => await models.AttributeValue.findAll(),
 	attributes: async (_, __, { models }) => await models.Attribute.findAll(),
 	categories: async (_, __, { models }) => await models.Category.findAll(),
-	category_products: async (_, { where }, { models }) =>
-		await models.Product.findAll({
+	category_products: async (_, { where, limit, offset }, { models }) => {
+		return await models.Product.findAll({
 			include: [{ as: 'ProductCategories', model: models.ProductCategory, where }],
+			limit,
+			offset,
 			required: true,
-		}),
+		});
+	},
 	customers: async (_, __, { models }) => await models.Customer.findAll(),
 	departments: async (_, __, { models }) => await models.Department.findAll(),
 	orders: async (_, __, { models }) => await models.Orders.findAll(),
