f199214cd36373171704a6f93062ca00ae6ebe78
Darko Pranjic <darkopranjic.4@gmail.com>
2019-03-07 23:36:42 +0100

    Prepare for social networks signup
    Add new fields, set password not reqired

--- src/migrations/20190307211225-password_field_length.js
+++ src/migrations/20190307211225-password_field_length.js
@@ -2,13 +2,6 @@
 
 module.exports = {
 	down: (queryInterface, Sequelize) => {
-		/*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
 		return queryInterface.changeColumn('customer', 'password', { type: Sequelize.STRING(50) });
 	},
 	up: (queryInterface, Sequelize) => {
--- src/models/customer.js
+++ src/models/customer.js
@@ -9,9 +9,16 @@ module.exports = (sequelize, DataTypes) => {
 		day_phone: DataTypes.STRING(100),
 		email: { allowNull: false, type: DataTypes.STRING(100), unique: true },
 		eve_phone: DataTypes.STRING(100),
+		facebook_id: DataTypes.STRING,
+		google_id: DataTypes.STRING, // id from google services
+		method: {
+			enum: ['local', 'google', 'facebook'],
+			required: true,
+			type: DataTypes.STRING(20),
+		},
 		mob_phone: DataTypes.STRING(100),
 		name: { allowNull: false, type: DataTypes.STRING(50) },
-		password: { allowNull: false, type: DataTypes.STRING(50) },
+		password: { type: DataTypes.STRING(50) },
 		postal_code: DataTypes.STRING(100),
 		region: DataTypes.STRING(100),
 		shipping_region_id: { allowNull: false, defaultValue: '1', type: DataTypes.INTEGER },
